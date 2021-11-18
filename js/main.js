26a4e032c25d12bbb755dad1eb75baa426a1a9aa
stefanofilippini <filippini.stefano96@gmail.com>
2021-11-18 01:43:11 +0100

    Non avevo sonno, minor bug fixed

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ var app = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
-                        text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
                     },
                     {
                         date: '10/01/2020 15:50:00',
@@ -26,10 +26,10 @@ var app = new Vue ({
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 16:15:22',
-                        text: 'Tutto fatto!',
-                        status: 'received'
-                    }
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
                 ],
             },
             {
@@ -38,8 +38,8 @@ var app = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: '20/03/2020 16:30:00',
-                        text: 'Ciao come stai?',
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                     },
                     {
@@ -48,10 +48,10 @@ var app = new Vue ({
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
-                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
                         status: 'sent'
-                    }
+                    },
                 ],
             },    {
                 name: 'Samuele',
@@ -59,8 +59,8 @@ var app = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: '28/03/2020 10:10:40',
-                        text: 'La Marianna va in campagna',
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
                         status: 'received'
                     },
                     {
@@ -69,10 +69,10 @@ var app = new Vue ({
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
-                        text: 'Ah scusa!',
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
                         status: 'received'
-                    }
+                    },
                 ],
             },
             {
@@ -81,15 +81,15 @@ var app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    },
+                    {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
-                    }
                 ],
             },
         ],
@@ -102,15 +102,15 @@ var app = new Vue ({
     methods : {
         setChat (index) {
             this.activeChat = index
-            this.scrollToEnd();
         },
 
         lastAccess(array) {
             let last = '';
             for (let i = 0; i < array.length; i++) {
                 if (array[i].status === 'received') {
-                   last = array[i].date;
-               } 
+                    last = array[i].date;
+                    break;
+                }
             }
             return last;
         },
@@ -121,31 +121,27 @@ var app = new Vue ({
         },
 
         nowDateTime() {
-            const now =dayjs().format('DD/MM/YYYY HH:mm:ss');
+            const now = dayjs().format('DD/MM/YYYY HH:mm:ss');
             return now;
         },
 
         addResponse (array) {
             //(function(){ alert("Hello"); }, 3000)
             setTimeout (() => {
-                array.push({
+                array.unshift({
                     date: this.nowDateTime(),
                     text: 'ok',
                     status: 'received'
                 })
-
-                //scroll to bottom
-                this.scrollToEnd();
             },
                 2000);
         },
 
         addMessage() {
             if (this.newMessage !=='') {
-                console.log(this.newMessage);
 
                 //aggiunta message in messages
-                this.contacts[this.activeChat].messages.push({
+                this.contacts[this.activeChat].messages.unshift({
                     date: this.nowDateTime(),
                     text: this.newMessage,
                     status: 'sent'
@@ -154,24 +150,12 @@ var app = new Vue ({
                 //add response
                 this.addResponse(this.contacts[this.activeChat].messages);
 
-                //scroll to bottom
-                this.scrollToEnd();
-
                 //clean up
                 this.newMessage='';
 
                 //set focus
-                console.log(this.$refs);
                 this.$refs.messageInput.focus()
             }
-        },
-
-        scrollToEnd() {
-            setTimeout(() => {
-                let container = document.querySelector(".overflow-scroll");
-                let scrollHeight = container.scrollHeight;
-                container.scrollTop = scrollHeight;
-            }, 0);
         },
     }
 })
